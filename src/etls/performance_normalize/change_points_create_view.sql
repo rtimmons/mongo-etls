fa1e20ea8247dd0fa31a8ff70e6f4d37530690e3
Ryan Timmons <ryan.timmons@10gen.com>
2021-07-21 13:29:37 -0400

    try moving the single dumb query to github

---
+++ src/etls/performance_normalize/change_points_create_view.sql
@@ -0,0 +1,26 @@
+--
+-- target: awsdatacatalog.dev_prod_metrics_v1.change_points
+-- 
+
+select 	time_series_info.project as "project",
+		time_series_info.variant as "variant",
+		time_series_info.task as "task",
+		time_series_info.test as "test",
+		time_series_info.measurement as "measurement",
+		version,
+		if(
+			   time_series_info.test like '%canary%'
+			or time_series_info.test like '%fio%'
+			or time_series_info.test like '%NetworkB%'
+			or time_series_info.test like '%Setup%'
+			or time_series_info.test like '%CleanUp%'
+			or time_series_info.test like '%ActorStarted%'
+			or time_series_info.test like '%ActorFinished%',
+			'yes', 'no'
+		)
+		test_matches_canary_rex,
+		date(triage.triaged_on) as triaged_on,
+		triage.triage_status as triage_status,
+		date(calculated_on) as calculated_on
+from 	dev_prod_performance_atlas.expanded_metrics.change_points
+where 	calculated_on between date('2012-09-16') and date(now())
---
+++ src/etls/performance_normalize/index.md
@@ -0,0 +1,10 @@
+Normalize Performance Data
+==========================
+
+Normalize `dev_prod_performance_atlas.expanded_metrics.change_points`
+
+Job link: https://mars.dataplatform.prod.corp.mongodb.com/app/job/9dbb4d51-648f-40df-90b4-37057525dd34
+
+Triggers:
+
+- Cron: `0 * * ? * *`
