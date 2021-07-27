b9ac79ec2011dc82b68777b49f46b094145a1327
Ryan Timmons <ryan.timmons@10gen.com>
2021-07-27 17:10:47 -0400

    mostly right comparing by hand but some subquery non-scalar thing happening

--- src/jobs/jira_bf_normalize/bf_stats/index.sql
+++ src/jobs/jira_bf_normalize/bf_stats/index.sql
@@ -1,3 +1,6 @@
+-- attempts to mimic 
+--   https://github.com/10gen/pm-resources/blob/411dcbbb4e02ab12c7a9f0af94e9427a53f80ae8/python_scripts/load_bf_data.py#L45
+-- in sql
 with
     histories as (
         select
@@ -28,39 +31,59 @@ with
                 issueid,
                 from_iso8601_timestamp(created) as created,
                 field,
-                tostring,
-                "from",
                 "fromstring",
-                "to",
-                -- TODO: use proper boolean types here
+                "tostring",
+                -- TODO: use proper boolean types here?
                 if(created = (
                     -- is first time going from "from" to "to"
                     select  min(created)
                     from    all_deltas ms
                     where   ms.field = ds.field
                     and     (
-                        (ms."from" is null and ds."from" is null)
+                        (ms."fromstring" is null and ds."fromstring" is null)
                         or
-                        (ms."from" is not null and 
-                         ds."from" is not null and
-                         ms."from" = ds."from")
+                        (ms."fromstring" is not null and 
+                         ds."fromstring" is not null and
+                         ms."fromstring" = ds."fromstring")
                     )
                     and     (
-                        (ms."to" is null and ds."to" is null)
+                        (ms."tostring" is null and ds."tostring" is null)
                         or
-                        (ms."to" is not null and 
-                         ds."to" is not null and
-                         ms."to" = ds."to")
+                        (ms."tostring" is not null and 
+                         ds."tostring" is not null and
+                         ms."tostring" = ds."tostring")
                     )
                     and     ms.issueid = ds.issueid
                 ), 'yes', 'no') as first_time_same_transition,
+
+                if(created = (
+                    -- is last time going from "from" to "to"
+                    select  max(created)
+                    from    all_deltas ms
+                    where   ms.field = ds.field
+                    and     (
+                        (ms."fromstring" is null and ds."fromstring" is null)
+                        or
+                        (ms."fromstring" is not null and 
+                         ds."fromstring" is not null and
+                         ms."fromstring" = ds."fromstring")
+                    )
+                    and     (
+                        (ms."tostring" is null and ds."tostring" is null)
+                        or
+                        (ms."tostring" is not null and 
+                         ds."tostring" is not null and
+                         ms."tostring" = ds."tostring")
+                    )
+                    and     ms.issueid = ds.issueid
+                ), 'yes', 'no') as last_time_same_transition,
                 if(created = (
                     -- is first time going from "from" to "to"
                     select  min(created)
                     from    all_deltas ms
                     where   ms.field = ds.field
-                    and     ms."from" is null
-                    and     ms."to" is not null
+                    and     ms."fromstring" is null
+                    and     ms."tostring" is not null
                     and     ms.issueid = ds.issueid
                 ), 'yes', 'no') as first_time_not_null
         from    all_deltas ds
@@ -70,37 +93,51 @@ with
                 id                          as id,
                 key                         as key,
                 fields__status__name        as status,
-                fields__priority            as priority,
+                -- fields__priority            as priority,
                 fields__resolution__name    as resolution,
                 fields__assignee__name      as assignee,
                 fields__customfield_14278   as evg_projects,
-                if(fields__customfield_12950 like '%perf%',true,false) as on_perf_project,
-                try(cast(fields__customfield_15550 as bigint))  as score,
+                -- if(fields__customfield_12950 like '%perf%',true,false) as on_perf_project,
+                coalesce(
+                    try(cast(fields__customfield_15550 as bigint)),
+                    0
+                ) as score,
             
-                -- TODO: 'nan' values?
-                fields__customfield_20463 as severity_types,
+                /*
+                if(fields__customfield_20463='nan', null, fields__customfield_20463) as severity_types,
+                */
 
+                /*
                 (
-                    /* TODO fix based on serde */
+                    -- TODO fix based on serde
                     fields__customfield_20463 like '%Release Blocking%' and
                     fields__customfield_20463 not like '%Not Release Blocking%'
                 ) as is_release_blocker,
-        
+                */
+
+                /*
                 coalesce(
                     try(cast(fields__customfield_15550 as bigint)),
                     0
                 ) > 100 as is_hot,
+                */
         
+                /*
                 fields__customfield_14277 like '%-required%' as on_required_buildvariant,
                 fields__customfield_14277 like '%-suggested%'as on_suggested_buildvariant,
+                */
         
+                /*
                 (
                     fields__status__name = 'Closed' or
                     fields__status__name = 'Waiting for bug fix' or
                     fields__status__name = 'Resolved'
                 ) as is_investigated,
+                */
         
+                /*
                 from_iso8601_timestamp(fields__created) as created_date,
+                */
         
                 (
                     -- TODO: is this right?
@@ -113,6 +150,7 @@ with
                     and   cs."tostring" = 'Build Failures'
                     and   cs.first_time_same_transition = 'yes'
                 ) as converted_date, -- aka processed_date
+
                 (
                     select created
                     from  first_changeds cs
@@ -120,7 +158,7 @@ with
                     and   cs.field = 'assignee'
                     and   cs."tostring" is not null
                     and   cs.first_time_not_null = 'yes'
-                ) as team_assigned_date,
+                ) as first_team_assigned_date,
 
                 (
                     select created
@@ -128,7 +166,7 @@ with
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
                     and   cs."tostring" = 'In Progress'
-                    and   cs.first_time_not_null = 'yes'
+                    and   cs.last_time_same_transition = 'yes'
                 ) as began_investigation_date,
 
                 (
@@ -137,7 +175,7 @@ with
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
                     and   cs."tostring" = 'Waiting for bug fix'
-                    and   cs.first_time_same_transition = 'yes'
+                    and   cs.last_time_same_transition = 'yes'
                 ) as wfbf_date,
         
                 (
@@ -146,7 +184,7 @@ with
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
                     and   cs."tostring" = 'Stuck'
-                    and   cs.first_time_same_transition = 'yes'
+                    and   cs.last_time_same_transition = 'yes'
                 ) as stuck_date,
         
                 (
@@ -155,7 +193,7 @@ with
                     where cs.issueid = issues.id
                     and   cs.field = 'priority'
                     and   cs."tostring" = 'Trivial - P5'
-                    and   cs.first_time_same_transition = 'yes'
+                    and   cs.last_time_same_transition = 'yes'
                 ) as trivial_date,
                 
         
@@ -165,38 +203,26 @@ with
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
                     and   cs."tostring" = 'Closed'
-                    and   cs.first_time_same_transition = 'yes'
-                ) as closed_date,
+                    and   cs.last_time_same_transition = 'yes'
+                ) as closed_date
 
                 -- nice to have maybe?
+                /*
                 fields__customfield_14277 as buildvariant, -- TODO: serde
                 fields__customfield_16252 as failure_type,
                 fields__customfield_12950 as evg_task,
                 fields__customfield_16350 as count_of_linked_failures
+                */
         from
             stagingawsdatacatalog.raw_jira.dw__jira__issues issues
     )
 select
     aissues.*,
-    team_assigned_date - converted_date as time_buildbaron_triage,
-    began_investigation_date - team_assigned_date as time_to_begin_investigation,
-    wfbf_date - began_investigation_date as time_investigated,
-    stuck_date - began_investigation_date as time_investigated_to_stuck,
-    trivial_date - began_investigation_date as time_investigated_to_trivial,
-    closed_date - wfbf_date as time_waiting_for_fix
+    to_milliseconds(first_team_assigned_date - converted_date) / 3600000 as time_buildbaron_triage,
+    to_milliseconds(began_investigation_date - first_team_assigned_date) / 3600000 as time_to_begin_investigation,
+    to_milliseconds(wfbf_date - began_investigation_date) / 3600000 as time_investigated,
+    to_milliseconds(stuck_date - began_investigation_date) / 3600000 as time_investigated_to_stuck,
+    to_milliseconds(trivial_date - began_investigation_date) / 3600000 as time_investigated_to_trivial,
+    to_milliseconds(closed_date - wfbf_date) / 3600000 as time_waiting_for_fix
 from aissues
-where 1=1
-    and aissues.id in(
-        '1451063',
-        '1448714',
-        '1448670',
-        '1448661',
-        '1448332',
-        '1447295',
-        '1446689',
-        '1446124',
-        '1445956',
-        '1445819'
-    );
-
-
+where aissues.key like 'BF%'
