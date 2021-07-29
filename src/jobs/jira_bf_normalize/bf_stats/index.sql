bb28a1fa8690ed18212e3ed19e5a8aee5e35b355
Ryan Timmons <ryan.timmons@10gen.com>
2021-07-29 16:41:07 -0400

    move distinct later for performance maybe, hack around evg_project parsing, abs around datediffs

--- src/jobs/jira_bf_normalize/bf_stats/index.sql
+++ src/jobs/jira_bf_normalize/bf_stats/index.sql
@@ -2,13 +2,33 @@
 --   https://github.com/10gen/pm-resources/blob/411dcbbb4e02ab12c7a9f0af94e9427a53f80ae8/python_scripts/load_bf_data.py#L45
 -- in sql
 with
+    /*
+    # find serverteam.yaml in 10gen/pm-resources
+    import yaml
+    aliases = dict()
+    with open("serverteam.yaml") as handle:
+        conts = yaml.load(handle)
+        for proper_name, data in conts.items():
+            person_aliases = data.get("aliases", [])
+            if not isinstance(person_aliases, list):
+                person_aliases = [person_aliases]
+            for alias in person_aliases:
+                aliases[alias] = {"name": proper_name, "team": data.get("team","unknown")}
+    for alias, data in aliases.items():
+        print(f"('{alias}', '{data['name']}', '{data['team']}'),")
+     */
+    people_aliases(alias, name, team) as (
+        values
+        ('backlog-server-repl', 'backlog-server-repl', 'replication')
+        -- the actual values aren't checked in!
+    ),
     histories_t as (
         select
             id,
             issueid,
             created,
             -- TODO: any better to put this in the lower `histories` view?
-            cast(json_parse(items) 
+            cast(json_parse(items)
                 as array(row(field varchar, toString varchar,
                                "from" varchar, fromString varchar, 
                                "to" varchar, fieldtype varchar))) as deltas,
@@ -34,8 +54,7 @@ with
                 unnest(deltas) as ds
     ),
     first_changeds as (
-        select  distinct -- some jira bugs e.g. 143693
-                issueid,
+        select  issueid,
                 from_iso8601_timestamp(created) as created,
                 field,
                 "fromstring",
@@ -112,30 +131,55 @@ with
     ),
     aissues_t as (
         select  
-                id                          as id,
+                id                          as issue_id,
                 key                         as key,
                 fields__status__name        as status,
                 -- fields__priority            as priority,
                 fields__resolution__name    as resolution,
                 fields__assignee__name      as assignee,
-                fields__customfield_14278   as evg_projects,
+
+                cast(json_parse(
+                    replace(replace(replace(replace(replace(
+                        fields__customfield_14278,
+                        '''', '"'),
+                        'True','true'),
+                        'False','false'),
+                        'None', '[]'),
+                        'nan', '[]')
+                ) as array(varchar)) as evg_projects,
+
                 -- if(fields__customfield_12950 like '%perf%',true,false) as on_perf_project,
                 coalesce(
                     try(cast(fields__customfield_15550 as bigint)),
                     0
                 ) as score,
-            
-                /*
-                if(fields__customfield_20463='nan', null, fields__customfield_20463) as severity_types,
-                */
 
-                /*
+                fields__assignee__name as assignee,
+
                 (
-                    -- TODO fix based on serde
+                    select team
+                    from   people_aliases
+                    where  alias = fields__assignee__name
+                ) as team,
+
+
+                -- TODO fix based on serde (don't use `like` and nees to be deserialized)
+                cast(json_parse(
+                    replace(replace(replace(replace(replace(
+                        fields__customfield_20463,
+                        '''', '"'),
+                        'True','true'),
+                        'False','false'),
+                        'None', '[]'),
+                        'nan', '[]')
+                ) as array(row(
+                    self varchar, disabled boolean, id varchar, value varchar
+                ))) as severity,
+                (
                     fields__customfield_20463 like '%Release Blocking%' and
                     fields__customfield_20463 not like '%Not Release Blocking%'
                 ) as is_release_blocker,
-                */
+
 
                 /*
                 coalesce(
@@ -144,27 +188,19 @@ with
                 ) > 100 as is_hot,
                 */
         
-                /*
                 fields__customfield_14277 like '%-required%' as on_required_buildvariant,
                 fields__customfield_14277 like '%-suggested%'as on_suggested_buildvariant,
-                */
         
-                /*
                 (
                     fields__status__name = 'Closed' or
                     fields__status__name = 'Waiting for bug fix' or
                     fields__status__name = 'Resolved'
                 ) as is_investigated,
-                */
         
-                /*
                 from_iso8601_timestamp(fields__created) as created_date,
-                */
         
                 (
-                    -- TODO: is this right?
-                    -- showing as null for issueid=572968
-                    select created
+                    select distinct created
                     from  first_changeds cs
                     where cs.issueid = issues.id
                     and   cs.field = 'project'
@@ -173,7 +209,7 @@ with
                     and   cs.first_time_same_transition = 'yes'
                 ) as converted_date, -- aka processed_date
                 (
-                    select created
+                    select distinct created
                     from  first_changeds cs
                     where cs.issueid = issues.id
                     and   cs.field = 'assignee'
@@ -181,7 +217,7 @@ with
                     and   cs.first_time_not_null = 'yes'
                 ) as first_team_assigned_date,
                 (
-                    select created
+                    select distinct created
                     from  first_changeds cs
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
@@ -190,7 +226,7 @@ with
                 ) as began_investigation_date,
 
                 (
-                    select created
+                    select distinct created
                     from  first_changeds cs
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
@@ -199,7 +235,7 @@ with
                 ) as wfbf_date,
         
                 (
-                    select created
+                    select distinct created
                     from  first_changeds cs
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
@@ -208,7 +244,7 @@ with
                 ) as stuck_date,
         
                 (
-                    select created
+                    select distinct created
                     from  first_changeds cs
                     where cs.issueid = issues.id
                     and   cs.field = 'priority'
@@ -218,7 +254,7 @@ with
                 
         
                 (
-                    select created
+                    select distinct created
                     from  first_changeds cs
                     where cs.issueid = issues.id
                     and   cs.field = 'status'
@@ -245,12 +281,11 @@ with
     )
 select
     aissues.*,
-    to_milliseconds(first_team_assigned_date - converted_date) / 3600000 as time_buildbaron_triage,
-    to_milliseconds(began_investigation_date - first_team_assigned_date) / 3600000 as time_to_begin_investigation,
-    to_milliseconds(wfbf_date - began_investigation_date) / 3600000 as time_investigated,
-    to_milliseconds(stuck_date - began_investigation_date) / 3600000 as time_investigated_to_stuck,
-    to_milliseconds(trivial_date - began_investigation_date) / 3600000 as time_investigated_to_trivial,
-    to_milliseconds(closed_date - wfbf_date) / 3600000 as time_waiting_for_fix
+    abs(to_milliseconds(first_team_assigned_date - converted_date) / 60000) as time_buildbaron_triage_mins,
+    abs(to_milliseconds(began_investigation_date - first_team_assigned_date) / 60000) as time_to_begin_investigation_mins,
+    abs(to_milliseconds(began_investigation_date - wfbf_date) / 60000) as time_investigated_mins,
+    abs(to_milliseconds(began_investigation_date - stuck_date) / 60000) as time_investigated_to_stuck_mins,
+    abs(to_milliseconds(began_investigation_date - trivial_date) / 60000) as time_investigated_to_trivial_mins,
+    abs(to_milliseconds(closed_date - wfbf_date) / 60000) as time_waiting_for_fix_mins
 from aissues
 where aissues.key like 'BF%'
-
