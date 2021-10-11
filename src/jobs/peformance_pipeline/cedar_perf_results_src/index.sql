52947c0f33b5df32be2688f49990bb94aa037e56
Ryan Timmons <ryan.timmons@10gen.com>
2021-10-11 16:56:54 -0400

    make _id explicit

--- src/jobs/peformance_pipeline/cedar_perf_results_src/index.sql
+++ src/jobs/peformance_pipeline/cedar_perf_results_src/index.sql
@@ -3,6 +3,7 @@
 -- awsdatacatalog.dev_prod_live.cedar_perf_results_src
 --
 
-SELECT  *,
+SELECT  _id,
+        *,
         localtimestamp as _extract_timestamp
 FROM    evergreen_cedar_atlas.cedar.perf_results
