711946d1689e1b50281156ce2a7ec344c1ad680c
Ryan Timmons <ryan.timmons@10gen.com>
2021-10-11 17:12:39 -0400

    cast _id to varchar in src extracts

--- src/jobs/peformance_pipeline/cedar_perf_results_src/index.sql
+++ src/jobs/peformance_pipeline/cedar_perf_results_src/index.sql
@@ -3,11 +3,11 @@
 -- awsdatacatalog.dev_prod_live.cedar_perf_results_src
 --
 
-SELECT  ps."_id"            AS "_id",
-        ps."info"           AS "info",
-        ps."created_at"     AS "created_at",
-        ps."completed_at"   AS "completed_at",
-        ps."rollups"        AS "rollups",
-        ps."analysis"       AS "analysis",
-        LOCALTIMESTAMP      AS "_extract_timestamp"
-FROM    evergreen_cedar_atlas.cedar.perf_results as ps
+SELECT  CAST(ps."_id" AS VARCHAR)   AS "_id",
+        ps."info"                   AS "info",
+        ps."created_at"             AS "created_at",
+        ps."completed_at"           AS "completed_at",
+        ps."rollups"                AS "rollups",
+        ps."analysis"               AS "analysis",
+        LOCALTIMESTAMP              AS "_extract_timestamp"
+FROM    evergreen_cedar_atlas.cedar.perf_results AS ps
--- src/jobs/peformance_pipeline/performance_expanded_metrics_time_series_src/index.sql
+++ src/jobs/peformance_pipeline/performance_expanded_metrics_time_series_src/index.sql
@@ -3,15 +3,15 @@
 -- awsdatacatalog.dev_prod_live.performance_expanded_metrics_time_series_src
 --
 
-SELECT  "_id"                     AS "_id",
-        "measurement"             AS "measurement",
-        "project"                 AS "project",
-        "task"                    AS "task",
-        "test"                    AS "test",
-        "variant"                 AS "variant",
-        "data"                    AS "data",
-        "updatefailures"          AS "updatefailures",
-        "lastsuccessfulupdate"    AS "lastsuccessfulupdate",
-        "lastupdateattempt"       AS "lastupdateattempt",
-        LOCALTIMESTAMP            AS "_extract_timestamp"
-FROM dev_prod_performance_atlas.expanded_metrics.time_series
+SELECT  CAST(ts."_id" AS VARCHAR)   AS "_id",
+        ts."measurement"             AS "measurement",
+        ts."project"                 AS "project",
+        ts."task"                    AS "task",
+        ts."test"                    AS "test",
+        ts."variant"                 AS "variant",
+        ts."data"                    AS "data",
+        ts."updatefailures"          AS "updatefailures",
+        ts."lastsuccessfulupdate"    AS "lastsuccessfulupdate",
+        ts."lastupdateattempt"       AS "lastupdateattempt",
+        LOCALTIMESTAMP              AS "_extract_timestamp"
+FROM dev_prod_performance_atlas.expanded_metrics.time_series AS ts
