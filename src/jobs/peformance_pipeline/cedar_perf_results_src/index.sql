ce46435cf40ef8630930b1de0ee4d8b7e46c44ec
Ryan Timmons <ryan.timmons@10gen.com>
2021-10-11 17:07:16 -0400

    supply full list of columns

--- src/jobs/peformance_pipeline/cedar_perf_results_src/index.sql
+++ src/jobs/peformance_pipeline/cedar_perf_results_src/index.sql
@@ -3,7 +3,11 @@
 -- awsdatacatalog.dev_prod_live.cedar_perf_results_src
 --
 
-SELECT  rs."_id"        AS "_id",
-        *,
-        LOCALTIMESTAMP  AS _extract_timestamp
-FROM    evergreen_cedar_atlas.cedar.perf_results AS rs
+SELECT  ps."_id"            AS "_id",
+        ps."info"           AS "info",
+        ps."created_at"     AS "created_at",
+        ps."completed_at"   AS "completed_at",
+        ps."rollups"        AS "rollups",
+        ps."analysis"       AS "analysis",
+        LOCALTIMESTAMP      AS "_extract_timestamp"
+FROM    evergreen_cedar_atlas.cedar.perf_results as ps
--- src/jobs/peformance_pipeline/performance_expanded_metrics_time_series_src/index.sql
+++ src/jobs/peformance_pipeline/performance_expanded_metrics_time_series_src/index.sql
@@ -3,7 +3,15 @@
 -- awsdatacatalog.dev_prod_live.performance_expanded_metrics_time_series_src
 --
 
-SELECT  ts."_id"        AS "_id",
-        *,
-        LOCALTIMESTAMP  AS "_extract_timestamp"
-FROM    dev_prod_performance_atlas.expanded_metrics.time_series AS ts
+SELECT  "_id"                     AS "_id",
+        "measurement"             AS "measurement",
+        "project"                 AS "project",
+        "task"                    AS "task",
+        "test"                    AS "test",
+        "variant"                 AS "variant",
+        "data"                    AS "data",
+        "updatefailures"          AS "updatefailures",
+        "lastsuccessfulupdate"    AS "lastsuccessfulupdate",
+        "lastupdateattempt"       AS "lastupdateattempt",
+        LOCALTIMESTAMP            AS "_extract_timestamp"
+FROM dev_prod_performance_atlas.expanded_metrics.time_series
