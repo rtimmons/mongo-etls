2aca2b3733b3248f5b371673bbc0ac506570f6d5
Ryan Timmons <ryan.timmons@10gen.com>
2021-10-11 15:16:00 -0400

    add xform_cedar_perf_results_stats

---
+++ src/jobs/peformance_pipeline/xform_cedar_perf_results_stats/index.sql
@@ -0,0 +1,23 @@
+--
+-- Target:
+-- awsdatacatalog.dev_prod_live.xform_cedar_perf_results_stats
+--
+-- Dependencies:
+--
+--   1. cedar_perf_results_src
+--
+--
+SELECT  ps."_id"                    AS perf_result_id,
+        ps."info"                   AS perf_result_info,
+        ps."created_at"             AS perf_result_created_at,
+        ps."completed_at"           AS perf_result_completed_at,
+        ps."_extract_timestamp"     AS _perf_result_extract_timestamp,
+        ps.rollups."processed_at"   AS rollup_processed_at,
+        ps.rollups."count"          AS rollups_count,
+        ps.rollups."valid"          AS rollups_valid,
+        ps."analysis"               AS rollups_analysis,
+        stats."name"                AS stat_name,
+        stats."val"                 AS stat_value,
+        CURRENT_TIMESTAMP           AS _extract_timestamp 
+FROM    awsdatacatalog.dev_prod_live.cedar_perf_results_src AS ps,
+        unnest(ps.rollups.stats)                            AS stats
