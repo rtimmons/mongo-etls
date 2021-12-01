2e0f3183c5c0eb7f08bb29091c509965a892f53c
Ryan Timmons <ryan.timmons@10gen.com>
2021-12-01 14:08:16 -0500

    well this is a mess so far

--- dag_script/performance_pipeline/__init__.py
+++ dag_script/performance_pipeline/__init__.py
@@ -15,15 +15,15 @@ def read_file(*relative_to_self):
     with open(to_read) as handle:
         return handle.read()
 
-
 task_1 = PrestoTask(
     name="materialize dev_prod_performance_atlas.evgdw.build_failures",
     conn_id="2cc4534c-7634-433f-8e01-8fb0ad19e282",
     sql_source="config",
-    sql=read_file("cedar_perf_results_src", "index.sql"),
+    sql=read_file("../src/jobs/peformance_pipeline/cedar_perf_results_src/index.sql"),
     destination=PrestoTableDestination(
         dest_tgt="awsdatacatalog.dev_prod_live.performance_evgdw_build_vailures",
         dest_replace=True,
+        dest_format="table",
     )
 )
 
