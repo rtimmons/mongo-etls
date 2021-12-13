77c4e860aba7dbb5efafd83512469a0bc08349e5
Ryan Timmons <ryan.timmons@10gen.com>
2021-12-13 11:39:15 -0500

    refactor

--- src/jobs/helpers.py
+++ src/jobs/helpers.py
@@ -1,7 +1,32 @@
+from mars_util.task import PrestoTask
+from mars_util.task.destination import PrestoTableDestination
+
+import os.path
+
 import jobs.whereami
+
+PRESTO_CONN = "920d5dfe-33ba-402a-b3ed-67ba21c25582"
 
 
 def read_file(*repo_rooted_path):
     to_read = jobs.whereami.repo_path(*repo_rooted_path)
     with open(to_read) as handle:
         return handle.read()
+
+
+class ConventionalPrestoTask(PrestoTask):
+    def __init__(self, name: str, file_path: str, **other_args):
+        args = {
+            "name": name,
+            "conn_id": PRESTO_CONN,
+            "sql_source": "config",
+            "destination": PrestoTableDestination(
+                dest_tgt=f"awsdatacatalog.dev_prod_live.{name}",
+                dest_replace=True,
+                dest_format="table",
+            ),
+        }
+        if "sql" not in other_args:
+            args["sql"] = read_file(os.path.dirname(file_path), f"{name}.sql")
+        args.update(other_args)
+        super().__init__(**args)
--- src/jobs/materialize_performance_and_cedar/__mars__.py
+++ src/jobs/materialize_performance_and_cedar/__mars__.py
@@ -3,72 +3,34 @@ By convention we expose the _DAG object in a __mars__.py file.
 """
 
 from mars_util.job_dag import JobDAG
-from mars_util.task import PrestoTask
-from mars_util.task.destination import PrestoTableDestination
 
-import jobs.helpers as helpers
+from jobs.helpers import ConventionalPrestoTask
 
-
-PRESTO_CONN = "920d5dfe-33ba-402a-b3ed-67ba21c25582"
-
-
-perf_results = PrestoTask(
+perf_results = ConventionalPrestoTask(
     name="perf_results",
-    conn_id=PRESTO_CONN,
-    sql_source="config",
-    sql=helpers.read_file("src", "jobs", "materialize_performance_and_cedar", "perf_results.sql"),
-    destination=PrestoTableDestination(
-        dest_tgt="awsdatacatalog.dev_prod_live.perf_results",
-        dest_replace=True,
-        dest_format="table",
-    )
+    file_path=__file__,
 )
-perf_results_unrolled = PrestoTask(
+perf_results_unrolled = ConventionalPrestoTask(
     name="perf_results_unrolled",
-    conn_id=PRESTO_CONN,
-    sql_source="config",
-    sql=helpers.read_file("src", "jobs", "materialize_performance_and_cedar", "perf_results_unrolled.sql"),
-    destination=PrestoTableDestination(
-        dest_tgt="awsdatacatalog.dev_prod_live.perf_results_unrolled",
-        dest_replace=True,
-        dest_format="table",
-    )
+    file_path=__file__,
 )
 # DependsOn
 perf_results_unrolled.set_prev(perf_results)
 
 
-time_series = PrestoTask(
+time_series = ConventionalPrestoTask(
     name="time_series",
-    conn_id=PRESTO_CONN,
-    sql_source="config",
-    sql=helpers.read_file("src", "jobs", "materialize_performance_and_cedar", "time_series.sql"),
-    destination=PrestoTableDestination(
-        dest_tgt="awsdatacatalog.dev_prod_live.time_series",
-        dest_replace=True,
-        dest_format="table",
-    )
+    file_path=__file__,
 )
-time_series_unrolled = PrestoTask(
+time_series_unrolled = ConventionalPrestoTask(
     name="time_series_unrolled",
-    conn_id=PRESTO_CONN,
-    sql_source="config",
-    sql=helpers.read_file("src", "jobs", "materialize_performance_and_cedar", "time_series_unrolled.sql"),
-    destination=PrestoTableDestination(
-        dest_tgt="awsdatacatalog.dev_prod_live.time_series_unrolled",
-        dest_replace=True,
-        dest_format="table",
-    )
+    file_path=__file__,
 )
 # DependsOn
 time_series_unrolled.set_prev(time_series)
 
 dag = JobDAG()
-dag.register_tasks([
-    perf_results,
-    perf_results_unrolled,
-    time_series,
-    time_series_unrolled
-])
+dag.register_tasks(
+    [perf_results, perf_results_unrolled, time_series, time_series_unrolled]
+)
 _DAG = dag
-
