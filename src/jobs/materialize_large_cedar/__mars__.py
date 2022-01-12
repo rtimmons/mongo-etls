e11c6f3673074537b06bffaf42c99c3f4ea5f4ef
Ryan Timmons <ryan.timmons@10gen.com>
2022-01-12 11:35:36 -0500

    add cedar.test_results and generate sqls from structure.yml

--- src/jobs/materialize_large_cedar/__mars__.py
+++ src/jobs/materialize_large_cedar/__mars__.py
@@ -9,6 +9,7 @@ helper = DagHelper(__file__)
 cedar__cedar__buildlogs__raw = helper.add_task("cedar__cedar__buildlogs__raw")
 cedar__cedar__historical_test_data__raw = helper.add_task("cedar__cedar__historical_test_data__raw")
 cedar__cedar__system_metrics__raw = helper.add_task("cedar__cedar__system_metrics__raw")
+cedar__cedar__test_results__raw = helper.add_task("cedar__cedar__test_results__raw")
 
 
 _DAG = helper.extract()
