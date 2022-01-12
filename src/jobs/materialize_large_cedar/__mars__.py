9bc0638d0e96c9924d9be02c57ead5f04054b650
Ryan Timmons <ryan.timmons@10gen.com>
2022-01-12 11:38:30 -0500

    notionally make the helpers _HELPER. Maybe help with docs?

--- src/jobs/materialize_large_cedar/__mars__.py
+++ src/jobs/materialize_large_cedar/__mars__.py
@@ -4,12 +4,12 @@ By convention we expose the _DAG object in a __mars__.py file.
 
 from src.jobs.helpers import DagHelper
 
-helper = DagHelper(__file__)
+_HELPER = DagHelper(__file__)
 
-cedar__cedar__buildlogs__raw = helper.add_task("cedar__cedar__buildlogs__raw")
-cedar__cedar__historical_test_data__raw = helper.add_task("cedar__cedar__historical_test_data__raw")
-cedar__cedar__system_metrics__raw = helper.add_task("cedar__cedar__system_metrics__raw")
-cedar__cedar__test_results__raw = helper.add_task("cedar__cedar__test_results__raw")
+cedar__cedar__buildlogs__raw = _HELPER.add_task("cedar__cedar__buildlogs__raw")
+cedar__cedar__historical_test_data__raw = _HELPER.add_task("cedar__cedar__historical_test_data__raw")
+cedar__cedar__system_metrics__raw = _HELPER.add_task("cedar__cedar__system_metrics__raw")
+cedar__cedar__test_results__raw = _HELPER.add_task("cedar__cedar__test_results__raw")
 
 
-_DAG = helper.extract()
+_DAG = _HELPER.extract()
--- src/jobs/materialize_performance_and_cedar/__mars__.py
+++ src/jobs/materialize_performance_and_cedar/__mars__.py
@@ -4,34 +4,34 @@ By convention we expose the _DAG object in a __mars__.py file.
 
 from src.jobs.helpers import DagHelper
 
-helper = DagHelper(__file__)
+_HELPER = DagHelper(__file__)
 
 
-cedar__cedar__perf_results__raw = helper.add_task("cedar__cedar__perf_results__raw")
-cedar__cedar__perf_results__raw__unrolled = helper.add_task(
+cedar__cedar__perf_results__raw = _HELPER.add_task("cedar__cedar__perf_results__raw")
+cedar__cedar__perf_results__raw__unrolled = _HELPER.add_task(
     "cedar__cedar__perf_results__raw__unrolled"
 )
 cedar__cedar__perf_results__raw__unrolled.set_prev(cedar__cedar__perf_results__raw)
 
-cedar__cedar__test_results__raw = helper.add_task("cedar__cedar__test_results__raw")
+cedar__cedar__test_results__raw = _HELPER.add_task("cedar__cedar__test_results__raw")
 
-performance__expanded_metrics__change_points__raw = helper.add_task(
+performance__expanded_metrics__change_points__raw = _HELPER.add_task(
     "performance__expanded_metrics__change_points__raw"
 )
 
-performance__expanded_metrics__time_series__raw = helper.add_task(
+performance__expanded_metrics__time_series__raw = _HELPER.add_task(
     "performance__expanded_metrics__time_series__raw"
 )
-performance__expanded_metrics__time_series__raw__unrolled = helper.add_task(
+performance__expanded_metrics__time_series__raw__unrolled = _HELPER.add_task(
     "performance__expanded_metrics__time_series__raw__unrolled"
 )
 performance__expanded_metrics__time_series__raw__unrolled.set_prev(
     performance__expanded_metrics__time_series__raw
 )
 
-performance__expanded_metrics__versions__raw = helper.add_task(
+performance__expanded_metrics__versions__raw = _HELPER.add_task(
     "performance__expanded_metrics__versions__raw"
 )
 
 
-_DAG = helper.extract()
+_DAG = _HELPER.extract()
