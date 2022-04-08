47832555c9a4e608dc90887a0db5e83c19e1e7cd
julianedwards <julian.edwards@10gen.com>
2022-04-08 17:25:04 -0400

    EVG-16140: add delete step to materialize_historical_test_data job (#13)

--- src/jobs/dev_prod/materialize_historical_test_data/__mars__.py
+++ src/jobs/dev_prod/materialize_historical_test_data/__mars__.py
@@ -2,6 +2,8 @@
 By convention we expose the _DAG object in a __mars__.py file.
 """
 
+from datetime import datetime, timedelta
+
 from src.jobs.parser import parse_job_config
 from src.jobs.variable import get_mars_var
 from src.jobs.whereami import repo_path
@@ -14,5 +16,7 @@ _DAG = parse_job_config(
         "presto_conn_id": get_mars_var("presto_conn_id"),
         "github_conn_id": get_mars_var("github_conn_id"),
         "mdb_conn_id": get_mars_var("mdb_conn_id"),
+        "environment": get_mars_var("environment"),
+        "date": f"{datetime.now().date() - timedelta(days=1)}",
     },
 )
